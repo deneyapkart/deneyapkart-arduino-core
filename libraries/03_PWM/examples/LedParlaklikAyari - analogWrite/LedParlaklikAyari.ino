fe6c3617261b5f5efc9452339c0a879ed33120fe
Ozgur BOSTAN <ozgur.bostan@rftek.com.tr>
2021-06-03 14:28:32 +0300

    Add new library and example

---
+++ libraries/03_PWM/examples/LedParlaklikAyari - analogWrite/LedParlaklikAyari.ino
@@ -0,0 +1,24 @@
+#include "deneyap.h"
+#include "analogWrite.h"
+
+#define LED_PIN   LEDB                              // Led pini (PWM cikis pini) 
+
+uint8_t brightness = 0;                             // Ledin parlaklik ayari (0-255)
+uint8_t brightStep = 5;                             // Parlaklik degisim adimi
+
+void setup() {
+  analogWriteChannel(LED_PIN);                      // PWM pin ayari   
+}
+
+void loop() {
+  brightness += brightStep;                         // Parlaklik kademesi degistiriliyor
+
+  if ( brightness == 0 || brightness == 255 )       // Sinir degerlerde parlaklik yonu degistiriliyor
+  {
+    brightStep = -brightStep;
+  }
+
+  analogWrite(LED_PIN, brightness);                 // Parlaklik degeri pin ile iliskilendiriliyor
+  
+  delay(50);
+}
