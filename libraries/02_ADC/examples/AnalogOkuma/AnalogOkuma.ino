38af23cf0f144309707682e2a0fdb12ab26c94e3
Ozgur BOSTAN <ozgur.bostan@rftek.com.tr>
2021-08-30 14:51:34 +0300

    Update AnalogOkuma.ino

--- libraries/02_ADC/examples/AnalogOkuma/AnalogOkuma.ino
+++ libraries/02_ADC/examples/AnalogOkuma/AnalogOkuma.ino
@@ -3,12 +3,17 @@ float adcVolDeg = 0;                                    // Voltaj donusumu icin 
 
 void setup() {
   Serial.begin(115200);                                 // Seri haberlesme baslatildi
-  pinMode(A0, INPUT);                                   // AO pini giris olarak ayarlandi
+  pinMode(A5, INPUT);                                   // AO pini giris olarak ayarlandi
 }
 
 void loop() {
-  adcHamVeri = analogRead(A0);                          // Ham veri okundu
+  adcHamVeri = analogRead(A5);                          // Ham veri okundu
+
+#if DENEYAP_KART
   adcVolDeg = (adcHamVeri * 3.3) / 4095;                // Voltaj donusumu yapildi
+#elif DENEYAP_MINI
+  adcVolDeg = (adcHamVeri * 3.3) / 8191;                // Voltaj donusumu yapildi
+#endif
 
   Serial.printf("\nADC ham veri\t: %d\n", adcHamVeri);  // Ham veri seri terminale yazdirildi
   Serial.printf("Voltaj degeri\t: %.2f", adcVolDeg);    // Voltaj degeri seri terminale yazdirildi
