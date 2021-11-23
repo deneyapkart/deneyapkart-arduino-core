3fb259933ed2eee14b4016b3c49ab24b6492a903
zeynepdicle <zeynep.diclee@hotmail.com>
2021-11-23 11:22:07 +0300

    Add new libraries and sketches

---
+++ libraries/Timer/examples/WatchdogTimer/WatchdogTimer.ino
@@ -0,0 +1,40 @@
+#include "esp_system.h"
+
+const int wdtTimeout = 3000;  //watchdog'u tetiklemek için ms cinsinden süre
+hw_timer_t *timer = NULL;
+
+void ARDUINO_ISR_ATTR resetModule() {
+  ets_printf("reboot\n");
+  esp_restart();
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Setup konfigürasyon");
+
+  //Buton gecikmeyi tetiklemek için kullanılacak
+  pinMode(GPKEY, INPUT);                            // Genel amacli buton pini giris olarak ayarlandi. 
+  
+  timer = timerBegin(0, 80, true);                  //timer 0, div 80
+  timerAttachInterrupt(timer, &resetModule, true);  //callback ekle
+  timerAlarmWrite(timer, wdtTimeout * 1000, false); //zamanı ayarla
+  timerAlarmEnable(timer);                          //interrupt etkinleştir
+}
+
+void loop() {
+  Serial.println("Döngüde");
+
+  timerWrite(timer, 0); //timer sıfırla (watchdog besleme)
+  long loopTime = millis();
+  //düğmeye basılıyken, Timer'ı tetiklemek için 3 saniyeye kadar gecikme
+  while (!digitalRead(GPKEY)) {
+    Serial.println("Butona basıldı");
+    delay(500);
+  }
+  delay(1000); //simule et 
+  loopTime = millis() - loopTime;
+  
+  Serial.print("Döngü süresi = ");
+  Serial.println(loopTime); //3000 ms'in altında olmalı
+}
---
+++ libraries/Timer/examples/ZamanlayiciTimer/ZamanlayiciTimer.ino
@@ -0,0 +1,78 @@
+/*
+  Zamanlayıcı örneğini Timer
+
+  Bu örnek, ESP32'de donanım Timer'ın nasıl kullanılacağını gösterir. Zamanlayıcı onTimer'ı çağırır
+  her saniye işlev görür. Zamanlayıcı, (IO0)'a bağalanan BUT butonu ile durdurulabilir
+  (IO0).
+
+  */
+  
+#define BTN_STOP_ALARM  0  //Durdur butonunun tanımlanması
+
+hw_timer_t * timer = NULL;
+volatile SemaphoreHandle_t timerSemaphore;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+volatile uint32_t isrCounter = 0;
+volatile uint32_t lastIsrAt = 0;
+
+void ARDUINO_ISR_ATTR onTimer(){
+  // Sayacı artırın ve ISR'nin zamanını ayarlayın
+  portENTER_CRITICAL_ISR(&timerMux);
+  isrCounter++;
+  lastIsrAt = millis();
+  portEXIT_CRITICAL_ISR(&timerMux);
+  // Döngüde kontrol edebileceğimiz bir semaphore verin
+  xSemaphoreGiveFromISR(timerSemaphore, NULL);
+  // Bir çıkışı değiştirmek istiyorsanız, burada digitalRead/Write kullanmak güvenlidir.
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(BTN_STOP_ALARM, INPUT);  // BTN_STOP_ALARM giriş olarak ayarlandı
+
+  // Timer tetiklendiğinde bizi bilgilendirmek için semaphore oluştur
+  timerSemaphore = xSemaphoreCreateBinary();
+
+  // 4'ün 1. zamanlayıcısını kullanın (sıfırdan sayılır).
+  // Ön ölçekleyici için 80 bölücü ayarlayın (daha fazla bilgi için ESP32 Teknik Referans Kılavuzuna bakın)
+  timer = timerBegin(0, 80, true);
+
+  // Timerımıza onTimer fonksiyonunu ekleyin.
+  timerAttachInterrupt(timer, &onTimer, true);
+
+   // Alarmı her saniye onTimer işlevini çağıracak şekilde ayarlayın (mikrosaniye cinsinden değer)
+   // Alarmı tekrarla (üçüncü parametre)
+  timerAlarmWrite(timer, 1000000, true);
+
+  // Alarmı başlatın
+  timerAlarmEnable(timer);
+}
+
+void loop() {
+  // Timer tetiklendiyse
+  if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE){
+    uint32_t isrCount = 0, isrTime = 0;
+    // Kesinti sayısını ve zamanını okuyun
+    portENTER_CRITICAL(&timerMux);
+    isrCount = isrCounter;
+    isrTime = lastIsrAt;
+    portEXIT_CRITICAL(&timerMux);
+    // Yazdırın
+    Serial.print("onTimer numarası: ");
+    Serial.print(isrCount);
+    Serial.print(" 'de ");
+    Serial.print(isrTime);
+    Serial.println(" ms");
+  }
+  // Butona basıldıysa
+  if (digitalRead(BTN_STOP_ALARM) == LOW) {
+    // Timer hala çalışıyorsa
+    if (timer) {
+      // Timer'ı bitirin
+      timerEnd(timer);
+      timer = NULL;
+    }
+  }
+}
