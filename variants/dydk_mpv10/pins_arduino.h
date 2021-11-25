40919539bad6b4fa1e93d2a20169755b2030cea3
Dogus Cendek <55203951+DogushC@users.noreply.github.com>
2021-11-25 17:53:09 +0300

    Update Pin

--- variants/dydk_mpv10/pins_arduino.h
+++ variants/dydk_mpv10/pins_arduino.h
@@ -3,14 +3,16 @@
 
 #include <stdint.h>
 
-#define EXTERNAL_NUM_INTERRUPTS 20
-#define NUM_DIGITAL_PINS        24
+#define EXTERNAL_NUM_INTERRUPTS 16
+#define NUM_DIGITAL_PINS        40
 #define NUM_ANALOG_INPUTS       16
-#define NUM_CAPACITIVE_INPUTS   10
 
 #define analogInputToDigitalPin(p)  (((p)<20)?(esp32_adc2gpio[(p)]):-1)
 #define digitalPinToInterrupt(p)    (((p)<40)?(p):-1)
 #define digitalPinHasPWM(p)         (p < 34)
+
+//static const uint8_t LED_BUILTIN = 5;
+//#define BUILTIN_LED  LED_BUILTIN // backward compatibility
 
 static const uint8_t KEY_BUILTIN = 0;
 
@@ -25,87 +27,35 @@ static const uint8_t MOSI  = 5;
 static const uint8_t MISO  = 18;
 static const uint8_t SCK   = 19;
 
-static const uint8_t D0 = 23;
-static const uint8_t D1 = 22;
-static const uint8_t D2 = 1;
-static const uint8_t D3 = 3;
-static const uint8_t D4 = 21;
-static const uint8_t D5 = 19;
-static const uint8_t D6 = 18;
-static const uint8_t D7 = 5;
-static const uint8_t D8 = 0;
-static const uint8_t D9 = 2;
-static const uint8_t D10 = 4;
-static const uint8_t D11 = 15;
-static const uint8_t D12 = 13;
-static const uint8_t D13 = 12;
-static const uint8_t D14 = 14;
-static const uint8_t D15 = 27;
-static const uint8_t D16 = 26;
-static const uint8_t D17 = 25;
-static const uint8_t D18 = 33;
-static const uint8_t D19 = 32;
-static const uint8_t D20 = 35;
-static const uint8_t D21 = 34;
-static const uint8_t D22 = 39;
-static const uint8_t D23 = 36;
-
 static const uint8_t A0 = 36;
-static const uint8_t A1 = 39;
-static const uint8_t A2 = 34;
-static const uint8_t A3 = 35;
+static const uint8_t A3 = 39;
 static const uint8_t A4 = 32;
 static const uint8_t A5 = 33;
-static const uint8_t A6 = 25;
-static const uint8_t A7 = 26;
-static const uint8_t A8 = 0;
-static const uint8_t A9 = 2;
+static const uint8_t A6 = 34;
+static const uint8_t A7 = 35;
 static const uint8_t A10 = 4;
-static const uint8_t A11 = 15;
-static const uint8_t A12 = 13;
-static const uint8_t A13 = 12;
-static const uint8_t A14 = 14;
-static const uint8_t A15 = 27;
+static const uint8_t A11 = 0;
+static const uint8_t A12 = 2;
+static const uint8_t A13 = 15;
+static const uint8_t A14 = 13;
+static const uint8_t A15 = 12;
+static const uint8_t A16 = 14;
+static const uint8_t A17 = 27;
+static const uint8_t A18 = 25;
+static const uint8_t A19 = 26;
 
-static const uint8_t T0 = 32;
-static const uint8_t T1 = 33;
-static const uint8_t T2 = 27;
-static const uint8_t T3 = 14;
-static const uint8_t T4 = 12;
-static const uint8_t T5 = 13;
-static const uint8_t T6 = 15;
-static const uint8_t T7 = 4;
-static const uint8_t T8 = 2;
-static const uint8_t T9 = 0;
+static const uint8_t T0 = 4;
+static const uint8_t T1 = 0;
+static const uint8_t T2 = 2;
+static const uint8_t T3 = 15;
+static const uint8_t T4 = 13;
+static const uint8_t T5 = 12;
+static const uint8_t T6 = 14;
+static const uint8_t T7 = 27;
+static const uint8_t T8 = 33;
+static const uint8_t T9 = 32;
 
 static const uint8_t DAC1 = 25;
 static const uint8_t DAC2 = 26;
-
-static const uint8_t PWM0 = 23;
-static const uint8_t PWM1 = 22;
-static const uint8_t PWM2 = 1;
-static const uint8_t PWM3 = 3;
-static const uint8_t PWM4 = 21;
-static const uint8_t PWM5 = 19;
-static const uint8_t PWM6 = 18;
-static const uint8_t PWM7 = 5;
-static const uint8_t PWM8 = 0;
-static const uint8_t PWM9 = 2;
-static const uint8_t PWM10 = 4;
-static const uint8_t PWM11 = 15;
-static const uint8_t PWM12 = 13;
-static const uint8_t PWM13 = 12;
-static const uint8_t PWM14 = 14;
-static const uint8_t PWM15 = 27;
-static const uint8_t PWM16 = 26;
-static const uint8_t PWM17 = 25;
-static const uint8_t PWM18 = 33;
-static const uint8_t PWM19 = 32;
-
-static const uint8_t LEDR = 3;
-static const uint8_t LEDG = 1;
-static const uint8_t LEDB = 4;
-static const uint8_t GPKEY = 0;
-
 
 #endif /* Pins_Arduino_h */
--- variants/dym_mpv10/pins_arduino.h
+++ variants/dym_mpv10/pins_arduino.h
@@ -3,10 +3,9 @@
 
 #include <stdint.h>
 
-#define EXTERNAL_NUM_INTERRUPTS 21
-#define NUM_DIGITAL_PINS        21
-#define NUM_ANALOG_INPUTS       9
-#define NUM_CAPACITIVE_INPUTS   14
+#define EXTERNAL_NUM_INTERRUPTS 46
+#define NUM_DIGITAL_PINS        48
+#define NUM_ANALOG_INPUTS       20
 
 #define analogInputToDigitalPin(p)  (((p)<20)?(esp32_adc2gpio[(p)]):-1)
 #define digitalPinToInterrupt(p)    (((p)<48)?(p):-1)
@@ -25,81 +24,43 @@ static const uint8_t MOSI  = 40;
 static const uint8_t MISO  = 39;
 static const uint8_t SCK   = 38;
 
-static const uint8_t D0 = 44;
-static const uint8_t D1 = 43;
-static const uint8_t D2 = 42;
-static const uint8_t D3 = 41;
-static const uint8_t D4 = 40;
-static const uint8_t D5 = 39;
-static const uint8_t D6 = 38;
-static const uint8_t D7 = 37;
-static const uint8_t D8 = 36;
-static const uint8_t D9 = 26;
-static const uint8_t D10 = 21;
-static const uint8_t D11 = 18;
-static const uint8_t D12 = 17;
-static const uint8_t D13 = 0;
-static const uint8_t D14 = 16;
-static const uint8_t D15 = 13;
-static const uint8_t D16 = 12;
-static const uint8_t D17 = 11;
-static const uint8_t D18 = 10;
-static const uint8_t D19 = 9;
-static const uint8_t D20 = 8;
+static const uint8_t A0 = 1;
+static const uint8_t A1 = 2;
+static const uint8_t A2 = 3;
+static const uint8_t A3 = 4;
+static const uint8_t A4 = 5;
+static const uint8_t A5 = 6;
+static const uint8_t A6 = 7;
+static const uint8_t A7 = 8;
+static const uint8_t A8 = 9;
+static const uint8_t A9 = 10;
+static const uint8_t A10 = 11;
+static const uint8_t A11 = 12;
+static const uint8_t A12 = 13;
+static const uint8_t A13 = 14;
+static const uint8_t A14 = 15;
+static const uint8_t A15 = 16;
+static const uint8_t A16 = 17;
+static const uint8_t A17 = 18;
+static const uint8_t A18 = 19;
+static const uint8_t A19 = 20;
 
-static const uint8_t A0 = 8;
-static const uint8_t A1 = 9;
-static const uint8_t A2 = 10;
-static const uint8_t A3 = 11;
-static const uint8_t A4 = 12;
-static const uint8_t A5 = 13;
-static const uint8_t A6 = 16;
-static const uint8_t A7 = 17;
-static const uint8_t A8 = 18;
-
-static const uint8_t T0 = 8;
-static const uint8_t T1 = 9;
-static const uint8_t T2 = 10;
-static const uint8_t T3 = 11;
-static const uint8_t T4 = 12;
-static const uint8_t T5 = 13;
-static const uint8_t T6 = 14;
-static const uint8_t T7 = 1;
-static const uint8_t T8 = 2;
-static const uint8_t T9 = 3;
-static const uint8_t T10 = 4;
-static const uint8_t T11 = 5;
-static const uint8_t T12 = 6;
-static const uint8_t T13 = 7;
+static const uint8_t T1 = 1;
+static const uint8_t T2 = 2;
+static const uint8_t T3 = 3;
+static const uint8_t T4 = 4;
+static const uint8_t T5 = 5;
+static const uint8_t T6 = 6;
+static const uint8_t T7 = 7;
+static const uint8_t T8 = 8;
+static const uint8_t T9 = 9;
+static const uint8_t T10 = 10;
+static const uint8_t T11 = 11;
+static const uint8_t T12 = 12;
+static const uint8_t T13 = 13;
+static const uint8_t T14 = 14;
 
 static const uint8_t DAC1 = 17;
 static const uint8_t DAC2 = 18;
-
-static const uint8_t PWM0 = 42;
-static const uint8_t PWM1 = 41;
-static const uint8_t PWM2 = 40;
-static const uint8_t PWM3 = 39;
-static const uint8_t PWM4 = 38;
-static const uint8_t PWM5 = 37;
-static const uint8_t PWM6 = 36;
-static const uint8_t PWM7 = 26;
-static const uint8_t PWM8 = 21;
-static const uint8_t PWM9 = 18;
-static const uint8_t PWM10 = 17;
-static const uint8_t PWM11 = 0;
-static const uint8_t PWM12 = 16;
-static const uint8_t PWM13 = 13;
-static const uint8_t PWM14 = 12;
-static const uint8_t PWM15 = 11;
-static const uint8_t PWM16 = 10;
-static const uint8_t PWM17 = 9;
-static const uint8_t PWM18 = 8;
-static const uint8_t PWM19 = 44;
-static const uint8_t PWM20 = 43;
-
-static const uint8_t LEDR = 34;
-static const uint8_t LEDG = 33;
-static const uint8_t LEDB = 35;
-static const uint8_t GPKEY = 0;
 
 #endif /* Pins_Arduino_h */
