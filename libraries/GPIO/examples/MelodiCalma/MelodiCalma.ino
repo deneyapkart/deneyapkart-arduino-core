acad63bef8604c1b57ab7428737f162c869403cd
Zeynep Dicle <zeynep.diclee@hotmail.com>
2022-08-29 11:25:36 +0300

    update package json file and add sketch

---
+++ libraries/GPIO/examples/MelodiCalma/MelodiCalma.ino
@@ -0,0 +1,104 @@
+/*
+ *   Daha dün annemizin ninnisinin melodi verilerinin çalıştırılması örneği
+ *   Notalar -> https://github.com/deneyapkart/deneyapkart-arduino-core/blob/master/libraries/00_Tone/src/pitches.h
+ *   
+ */
+ 
+#include <Tone32.h>
+
+#define SpeakerPin A0
+#define Frequency 500
+
+void setup() {}
+
+void loop() {
+  tone(SpeakerPin, NOTE_C5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_C5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_G5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_G5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_A5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_A5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_G5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+
+  tone(SpeakerPin, NOTE_F5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_F5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_E5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_E5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_D5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_D5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_C5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+
+  tone(SpeakerPin, NOTE_G5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_G5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_F5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_F5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_E5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_E5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_D5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+
+  tone(SpeakerPin, NOTE_G5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_G5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_F5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_F5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_E5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_E5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_D5, Frequency, 0);
+  noTone(SpeakerPin, 0);  
+
+  tone(SpeakerPin, NOTE_C5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_C5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_G5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_G5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_A5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_A5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_G5, Frequency, 0);
+  noTone(SpeakerPin, 0);  
+
+  tone(SpeakerPin, NOTE_F5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_F5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_E5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_E5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_D5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_D5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+  tone(SpeakerPin, NOTE_C5, Frequency, 0);
+  noTone(SpeakerPin, 0);
+}
--- libraries/PWM/examples/LedParlaklikAyari/LedParlaklikAyari.ino
+++ libraries/PWM/examples/LedParlaklikAyari/LedParlaklikAyari.ino
@@ -5,7 +5,7 @@
  */
 #define PWM_FREQ  5000                                  // PWM frekansi
 #define PWM_CH    0                                     // PWM kanal secimi (0-15)
-#define LED_PIN   LEDB                                  // Led pini (PWM cikis pini) (PWM0-PWM1)
+#define LED_PIN   PWM0                                  // Led pini (PWM cikis pini) (PWM0-PWM1)  DENEYAP MİNİ:P0-P1
 #define PWM_RES   8                                     // PWM cozunurlugu (1-16 bit) 
 
 uint8_t ledBrightness = 0;                              // Ledin parlaklik ayari (0-255)
