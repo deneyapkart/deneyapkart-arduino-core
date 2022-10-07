3f4467404b2b896da678a97a8e09a257f380d251
Zeynep Dicle <zeynep.diclee@hotmail.com>
2022-10-07 09:39:26 +0300

    Update ServoMotor.ino

--- libraries/PWM/examples/ServoMotor/ServoMotor.ino
+++ libraries/PWM/examples/ServoMotor/ServoMotor.ino
@@ -6,7 +6,7 @@
  */
 #include "ServoESP32S2.h"
 
-#define servoPin D8
+#define servoPin D8 // SD pini
 Servo_ESP32S2 servo ={-1, servoPin};
 int position = 0;
 
@@ -17,11 +17,11 @@ void setup(){
 }
 
 void loop(){   
-  for (position = 0; position <= 180; position += 5){
+  for (position = 0; position <= 180; position += 30){
     ServoESP32S2.setPosition(servo.servoIndex, position);
     delay(50);
   }
-  for (position = 180; position >= 0; position -= 5){
+  for (position = 180; position >= 0; position -= 30){
     ServoESP32S2.setPosition(servo.servoIndex, position);
     delay(50);
   }
