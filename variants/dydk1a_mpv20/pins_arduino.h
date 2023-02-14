21022eb83b73dd4923ffb9b31e0ff25dde2e1bca
Dogus Cendek <55203951+DogushC@users.noreply.github.com>
2023-02-14 14:03:54 +0300

    Updated pin numbers of Deneyap Kart 1A v2
    Deleted BOOT pin.
    Fixed D12, D13, D14, D15, PWM0, PWM1 pin numbers.
    Added A8, T0, T1, T2, T3, T4, T5, T6, T7, T8, D16, D17, D18, D19, PWM2, PWM3, PWM4 and BAT pin numbers.

--- variants/dydk1a_mpv20/pins_arduino.h
+++ variants/dydk1a_mpv20/pins_arduino.h
@@ -28,7 +28,6 @@ static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT+48;
 static const uint8_t GPKEY  = 0;
 #define KEY_BUILTIN GPKEY
 #define BUILTIN_KEY GPKEY
-#define BOOT GPKEY
 
 static const uint8_t TX 	= 43;
 static const uint8_t RX 	= 44;
@@ -49,6 +48,17 @@ static const uint8_t A4 	= 15;
 static const uint8_t A5 	= 16;
 static const uint8_t A6 	= 17;
 static const uint8_t A7 	= 18;
+static const uint8_t A8 	= 9;
+
+static const uint8_t T0 	= 4;
+static const uint8_t T1 	= 5;
+static const uint8_t T2 	= 6;
+static const uint8_t T3 	= 7;
+static const uint8_t T4 	= 8;
+static const uint8_t T5 	= 3;
+static const uint8_t T6 	= 10;
+static const uint8_t T7 	= 1;
+static const uint8_t T8 	= 2;
 
 static const uint8_t D0 	= 1;
 static const uint8_t D1 	= 2;
@@ -62,13 +72,20 @@ static const uint8_t D8 	= 38;
 static const uint8_t D9 	= 48;
 static const uint8_t D10 	= 47;
 static const uint8_t D11 	= 21;
-static const uint8_t D12 	= 0;
-static const uint8_t D13 	= 10;
-static const uint8_t D14 	= 3;
-static const uint8_t D15 	= 8;
+static const uint8_t D12 	= 10;
+static const uint8_t D13 	= 3;
+static const uint8_t D14 	= 8;
+static const uint8_t D15 	= 0;
+static const uint8_t D16 	= 13;
+static const uint8_t D17 	= 12;
+static const uint8_t D18 	= 11;
+static const uint8_t D19 	= 14;
 
-static const uint8_t PWM0 	= 1;
-static const uint8_t PWM1 	= 2;
+static const uint8_t PWM0 	= 15;
+static const uint8_t PWM1 	= 16;
+static const uint8_t PWM2 	= 17;
+static const uint8_t PWM3 	= 18;
+static const uint8_t PWM4 	= 38;
 
 static const uint8_t CAMSD = 4;
 static const uint8_t CAMSC = 5;
@@ -89,5 +106,7 @@ static const uint8_t SDMI 	= 14;
 static const uint8_t SDMO 	= 12;
 static const uint8_t SDCS 	= 11;
 static const uint8_t SDCK 	= 13;
+
+static const uint8_t BAT	= 9;
 
 #endif /* Pins_Arduino_h */
